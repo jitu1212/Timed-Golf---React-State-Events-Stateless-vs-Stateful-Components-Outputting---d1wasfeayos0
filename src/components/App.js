c8939602511f5768381c1a519d18ac2085638855
jitu yadav <98046486+jitu1212@users.noreply.github.com>
2022-08-21 09:53:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,102 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = { 
+      time: 0, 
+      x: 0, 
+      y: 0,
+      renderBall: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.timmer = this.timmer.bind(this);
+
   }
+
+
   componentDidMount() {
-    
+        window.addEventListener('keydown', (e) => {
+          switch (e.key) {
+              case 'ArrowRight':
+                this.setState({
+                    x: this.state.x + 5,
+                    y: this.state.y + 0,
+                })
+                break;
+
+              case 'ArrowDown':
+                this.setState({
+                  x: this.state.x + 0,
+                  y: this.state.y + 5,
+                })
+                
+                break;
+
+              case 'ArrowLeft':
+                this.setState({
+                  x: this.state.x - 5,
+                  y: this.state.y + 0,
+                })
+                break;
+
+              case 'ArrowUp':
+                this.setState({
+                  x: this.state.x + 0,
+                  y: this.state.y - 5,
+                })
+                break;
+          }
+      });
+
+      
+      if(this.state.x == 250 && this.state.y == 250){
+        console.log("I AM FROM IF");
+        window.clearInterval(interval);
+      }
+
   }
 
   componentWillUnmount() {
-    
+    this.setState({
+      time : 0,
+      // renderBall: false,
+    })
   }
 
+  timmer(){
+
+    if(this.state.x!=250 && this.state.y!=250){
+      this.setState({
+        time: this.state.time + 1,
+      })
+    }
+    else{
+      
+    }
+  }
+
+
+  handleClick(){
+    // console.log("Start Button Clicked");
+    var interval = window.setInterval(this.timmer,1000);
+
+    this.setState({
+      renderBall : true,
+    })
+  }
 
 
   render() {
     return (
- <>
-</>
+      <>
+        <div className="ball" style={{left : this.state.x + "px" , top : this.state.y + "px"}}></div>
+        <div className="hole"></div>
+        <h1 className="heading-timer">{this.state.time}</h1>
+        <button className="start" onClick={this.handleClick}>Start</button>
+
+      </>
     );
   }
 }
 
 export default Timer;
+
